1b0fc7d653fa2b4c0613996195f9f251a2de14ef
Arthur Vinicius <acad.arthur.alm@gmail.com>
2024-05-03 15:30:06 -0300

    Versão 0.12

---
+++ front_end/index.js
@@ -0,0 +1,13 @@
+// Função para criar um timer na section promoção
+//var  temp = nome_variável = setTimout(timer, );
+
+
+// Função para fechar aba promocional
+
+let promo = document.getElementById('promocao');
+let fechar = document.getElementById('fechar_prom');
+fechar.addEventListener('click', () => close(promo));
+
+function close(vari) {
+    vari.style.display = "none";
+}
