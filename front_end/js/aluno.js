9b3d4c918628ffea720fc6db809132cb2201a5d5
Arthur Vinicius <acad.arthur.alm@gmail.com>
2024-05-16 13:38:22 -0300

    Versão 0.16 - Atualização e reorganização dos arquivos.

--- front_end/js/aluno.js
+++ front_end/js/aluno.js
@@ -1,7 +1,3 @@
-// Função para criar um timer na section promoção
-//var  temp = nome_variável = setTimout(timer, );
-
-
 // Função para fechar aba promocional
 
 let promo = document.getElementById('sessao_promocao');
@@ -11,3 +7,7 @@ fechar.addEventListener('click', () => close(promo));
 function close(vari) {
     vari.style.display = "none";
 }
+
+
+
+// TIMER
--- front_end/js/index.js
+++ front_end/js/index.js
@@ -2,6 +2,7 @@
 //var  temp = nome_variável = setTimout(timer, );
 
 
+
 // Função para fechar aba promocional
 
 let promo = document.getElementById('sessao_promocao');
